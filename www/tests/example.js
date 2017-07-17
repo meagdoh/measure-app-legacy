f9777a0aeacb38e9ae89932bec34bdadd2efe939
meagdoh <meagdoherty@gmail.com>
2017-07-17 14:20:07 -0400

    successfully run initial test

--- www/tests/example.js
+++ www/tests/example.js
@@ -1,4 +1,14 @@
+beforeEach(module('myApp'));
+
+
 describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+});
+
+
+describe('all libraries loaded', function() {
   it('should be true', function() {
     expect('foo').toBe('foo');
   });
