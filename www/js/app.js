2b22a85a629741d217f9b9c88c83fb9783f5841c
meagdoh <meagdoherty@gmail.com>
2017-07-17 17:07:21 -0400

    semi-working tests (1 of 2)

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,7 @@ angular.module('Measure', ['ionic', 'gettext', 'ngSanitize',
 // .constant('CLIENT_APPLICATION', CLIENT_APPLICATION)
 // .constant('CLIENT_VERSION', CLIENT_VERSION)
 // .constant('ENVIRONMENT_CAPABILITIES', ENVIRONMENT_CAPABILITIES)
-//
+
 .value('MeasureConfig', {
 	'environmentType': undefined,
 	'environmentCapabilities': {},
--- www/tests/example.js
+++ www/tests/example.js
@@ -1,5 +1,3 @@
-beforeEach(module('myApp'));
-
 
 describe('example test', function() {
   it('should be true', function() {
@@ -8,8 +6,17 @@ describe('example test', function() {
 });
 
 
-describe('all libraries loaded', function() {
-  it('should be true', function() {
-    expect('foo').toBe('foo');
+describe('HistoryCtrl', function() {
+  beforeEach(module('ionic'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe()
+  it("is ready to g- .CSV", function () {
+      expect(historicalData.measurements.length).toBeGreaterThan(0);
   });
 });
