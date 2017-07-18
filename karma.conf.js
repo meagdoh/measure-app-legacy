bc2ed57d072e69605fbebb152b3abc60758c0718
meagdoh <meagdoherty@gmail.com>
2017-07-18 11:46:34 -0400

    Add constants.js to Karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'www/js/constants.js',
       'www/js/app.js',
       'www/tests/*.js'
     ],
